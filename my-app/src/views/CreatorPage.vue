c2f88bd558b1f968be34715c7dcb015297b566d0
Vincent Dante <vincent.dante96@gmail.com>
2021-02-28 16:03:04 +0800

    Update

---
+++ my-app/src/views/CreatorPage.vue
@@ -0,0 +1,190 @@
+<template>
+
+  <div  v-show="!showLoadingPage" class="content-container">
+    <div class="container">
+      <div class="row g-2">
+        <div class="col-lg-4 mb-5">
+          <a href="#" @click.prevent="goBack()" class="a-link a-link-dash-after">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
+            </svg>
+            Back
+          </a>
+          <router-link to="/" class="a-link">Home</router-link>
+        </div>
+        </div>
+        <div class="row g-2">
+          <div class="col-md-4">
+            <img :src="thumbnail" alt="" srcset="" class="rounded shadow">
+          </div>
+        <div class="col-md-8">
+          <div class="container-description">
+            <h1>{{ fullName }}</h1>
+            <br>
+            <br>
+            <br>
+            <p class=""><small class="text-muted">Data provided by Marvel. © 2014 Marvel</small></p>
+          </div>
+        </div>
+      </div>
+
+	
+      <br>
+      <br>
+      <br>
+
+      <div class="row g-3">
+        <div class="col-lg-12">
+          <h2 class="h2-comics">Comics</h2>
+          <h5 v-show="showNoComicsFound" class="text-center">No comics found...</h5>
+        </div>
+
+
+        <div class="col-sm-12 col-md-6 col-lg-3" v-for="(comics, id) in characterComics" :key="id">
+
+          <div class="card shadow-sm mb-2 me-0 ms-0 text-start card-character" @click="getComics(comics.id)">
+            <div class="thumbnail-card-container">
+              <img v-if="comics.images.length == 0" :src="'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'" class="card-img-top thumbnail-card">
+              <img v-else :src="comics.images[0].path+'.'+comics.images[0].extension" alt="" class="card-img-top thumbnail-card">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">{{comics.title}}</h5>
+              <br>
+              <hr>
+              <span class="footer-text-box">Data provided by Marvel. © 2014 Marvel</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div v-show="showLoadingPage" class="holder">
+    <div class="preloader">
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { useRoute } from 'vue-router'
+
+const axios = require('axios');
+
+
+export default {
+  name: 'CreatorPage',
+  data() {
+    return {
+      showLoadingPage: true,
+      fullName: "",
+      thumbnail: ""
+    }
+  },
+  setup(){
+    const route = useRoute();
+    const id = route.params.id;
+
+    return {
+      id
+    };
+  },
+  mounted(){
+    this.showLoadingPage = true;
+    this.getCreator(this.id)
+
+  },
+  methods: {
+    getCreator(id){
+
+      axios
+      .get(`/characters/creator/${id}`)
+      .then( response => {
+        console.log(response)
+        let res = response.data;
+        this.fullName = res[0].fullName;
+        this.thumbnail = res[0].thumbnail.path+'.'+res[0].thumbnail.extension;
+
+
+        this.showLoadingPage = false;
+      })
+      .catch( err => console.error(err) )
+    },
+    goBack(){
+
+      this.$router.go(-1)
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .a-link {
+    font-style: italic;
+    font-size: 18px;
+    padding: 10px 5px 10px 0;
+    margin-right: 10px;
+    text-decoration: none;
+  }
+
+  .a-link-dash-after::after {
+    content: "/";
+    padding-left: 10px;
+  }
+
+  .container {
+    text-align: left;
+  }
+
+  img {
+    width: 100%;
+  }
+
+  .creator-link {
+    display: inline-block;
+    position: relative;
+    color: #fff;
+    background: #bb4447;
+    padding: 5px 5px 5px 10px !important;
+    margin: 20px 5px;
+    text-decoration: none;
+    min-width: 180px;
+    font-size: 15px;
+    cursor: pointer;
+    bottom: 0;
+    transition: bottom 0.3s ease-in-out;
+  }
+
+  .creator-link:hover,
+  .creator-link:focus
+  {
+    bottom: 8px;
+  }
+
+
+  .creator-link-span {
+    position: absolute;
+    bottom: 32px;
+    left: 0px;
+    background: #444;
+    padding: 0 5px;
+    font-size: 13px;
+    font-weight: bold;
+    color: #fff;
+  }
+
+  .container-description {
+    padding-top: 30px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    .container-description {
+      padding: 0 0 0 50px;
+    }
+  }
+
+</style>
